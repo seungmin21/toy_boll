f3c7495b3dfeb3e1c51ce9fc3d60c8f137a050b0
ohseungmin <ohmin2886@gmail.com>
2023-10-11 14:56:28 +0900

    if문의 조건을 지정하지 못한 상태

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const test = setInterval( () => {
-  console.log("확인")
-}, 1000);
+//const test = setInterval( () => {
+//  console.log("확인")
+//}, 1000);
+
+
+const testTwo = setInterval( () => {
+  //조건을 정하지 못하고 있는 상황
+  if (testTwo === ) {
+    console.log("실행");
+  } else {
+    console.log("조건을 다시");
+  }
+}, 1000)
